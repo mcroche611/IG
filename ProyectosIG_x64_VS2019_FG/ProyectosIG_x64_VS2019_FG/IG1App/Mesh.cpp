86697f0820e8c09d97493d148b453017bfc4bfdc
mcroche611 <69208187+mcroche611@users.noreply.github.com>
2022-03-04 16:17:29 +0100

    Pagina 3

--- ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/Mesh.cpp
+++ ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/Mesh.cpp
@@ -33,6 +33,13 @@ void Mesh::render() const
     glDisableClientState(GL_COLOR_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
   }
+
+  if (vTexCoords.size() > 0)
+  {
+      glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+      glTexCoordPointer(2, GL_DOUBLE, 0, vTexCoords.data());
+      glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+  }
 }
 //-------------------------------------------------------------------------
 
