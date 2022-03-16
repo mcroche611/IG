769e6fb9aaed34ea2078e02d93669af8e756a548
mcroche611 <69208187+mcroche611@users.noreply.github.com>
2022-03-16 17:42:33 +0100

    Estrella se renderiza dos veces para dos direcciones

--- ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/Estrella3D.cpp
+++ ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/Estrella3D.cpp
@@ -1,5 +1,6 @@
 #include "Estrella3D.h"
 #include "Mesh.h"
+#include <ext/matrix_transform.hpp>
 
 Estrella3D::Estrella3D(GLdouble re, GLdouble np, GLdouble h)
 {
@@ -20,6 +21,10 @@ void Estrella3D::render(dmat4 const& modelViewMat)const
         glPolygonMode(GL_FRONT, GL_LINE);
         glPolygonMode(GL_BACK, GL_LINE);
         dmat4 aMat = modelViewMat * mModelMat;
+        upload(aMat);
+        myMesh->render();
+        aMat = aMat * rotate(modelMat(), radians(180.0), dvec3(0, 1, 0));
+        
         upload(aMat);
         glLineWidth(2);
         myMesh->render();
