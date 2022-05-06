f63d50257b0ed65559febee5fd9486a723f1ba36
mcroche611 <mcroche@ucm.es>
2022-05-06 13:37:21 +0200

    cubo funciona

--- ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/CuboIndexado.cpp
+++ ProyectosIG_x64_VS2019_FG/ProyectosIG_x64_VS2019_FG/IG1App/CuboIndexado.cpp
@@ -1,6 +1,7 @@
 #include "CuboIndexado.h"
 #include "Mesh.h"
 #include "IndexMesh.h"
+#include <gtc/type_ptr.hpp>
 CuboIndexado::CuboIndexado(GLdouble longitud)
 {
     l_ = longitud;
@@ -17,7 +18,10 @@ void CuboIndexado::render(dmat4 const& modelViewMat)const
     {
         dmat4 aMat = modelViewMat * mModelMat;
         upload(aMat);
+        glEnable(GL_COLOR_MATERIAL);
+        glColor3f(mColor.r, mColor.g, mColor.b);
         myMesh->render();
+        glColor3f(1.0, 1.0, 1.0);
     }
 
 }
